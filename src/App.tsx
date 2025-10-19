c5a2322667ff79e6dc5fe3d89ad1062a2ee941d6
Yasmine <yasminegara99@gmail.com>
2025-10-20 00:15:13 +0200

    Interface chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { Button } from "@/components/ui/button"
+import { Chat } from "@/components/ui/chat"
+import { useState } from "react"
 
 function App() {
+  const [inputText, setInputText] = useState("")
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <Chat
+        className="max-w-5xl"
+        messages={[]}
+        input={inputText}
+        handleInputChange={(text) => setInputText(text.target.value)}
+        handleSubmit={() => { }}
+        isGenerating={false}
+        stop={stop}
+      />
     </div>
   )
 }
